a9be9c475a3d1547ad8cf255b07fd46c76fc4b98
Joel Terrell <falconreid@hotmail.com>
2020-10-22 10:22:18 -0600

    server file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const mongojs = require("mongojs");
+const databaseURL = "Workout";
+const collections = ["exercises"];
+const db = mongojs(databaseURL, collections);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
